526890f884caa4667144d8bff8b05fddbe9f7eb6
Djoeke Duijker <djoekeduijker@live.nl>
2024-10-21 11:09:19 +0200

    created indexes

---
+++ frontend/app/(tabs)/groups/index.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import { useTranslation } from "react-i18next";
+import { Text, View } from "react-native";
+import { useTheme } from "../../../context/ThemeContext";
+
+export default function GroupIndex() {
+  const { t } = useTranslation();
+  const { theme } = useTheme();
+  
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <Text style={{color: theme.colors.onBackground}}>{t("groups.title")}</Text>
+    </View>
+  );
+}
