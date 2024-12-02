c2e4c39ef06073ee5a246813c9859279cc9360e0
Djoeke Duijker <djoekeduijker@live.nl>
2024-12-02 22:05:05 +0100

    Stop tracking node_modules

--- node_modules/react-native-paper/lib/typescript/components/TextInput/Label/LabelBackground.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import type { LabelBackgroundProps } from '../types';
-declare const LabelBackground: ({ labeled, labelLayoutWidth, labelLayoutHeight, placeholderStyle, baseLabelTranslateX, topPosition, backgroundColor, roundness, labelStyle, maxFontSizeMultiplier, testID, }: LabelBackgroundProps) => (React.JSX.Element | null)[];
-export default LabelBackground;
-//# sourceMappingURL=LabelBackground.d.ts.map
